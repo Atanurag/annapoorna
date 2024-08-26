9c4a4d0cd77d2efcae0f1698ad036852c97e8797
Atanurag <anuragt9510@gmail.com>
2024-08-26 21:47:50 +0530

    eee

--- src/demo.js
+++ src/demo.js
@@ -491,7 +491,7 @@ function onBuyClicked() {
       data: {
         pa: '7875853859@pthdfc',
         pn: 'Anurag Tiwari-1',
-        tr: 'h28r3u549ryt8tr7Doeo',// your custom transaction reference ID
+        tr: 'h28r3u54e9ryt8tr7Doeo',// your custom transaction reference ID
         url:'https://17174cc3-e036-41c5-82a6-1ce90c624cd6-00-2oq5i07bzmsdh.pike.repslit.dev:5000',
         mc: '5812', // your merchant category code
       },
@@ -564,13 +564,17 @@ function showPaymentUI(request, canMakePayment) {
  
         window.clearTimeout(paymentTimeout);
         //alert(instrument);
-         let dataString = JSON.stringify(instrument);
-         fetch('https://c0ccd437-87bb-4fd4-b585-6ef2b6165e6e-00-xn5f3f0kqnav.sisko.replit.dev/'
-, {
-  method: 'POST',
-  headers: new Headers({'Content-Type': 'application/json'}),
-   body: JSON.stringify({k:instrument}),
-}).then(e=>e.json()).then(d=>console.log(d));
+        let dataString = JSON.stringify({k: instrument});
+        fetch('https://c0ccd437-87bb-4fd4-b585-6ef2b6165e6e-00-xn5f3f0kqnav.sisko.replit.dev/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: dataString,
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
         // const tezResponse = JSON.parse(dataString.details.tezResponse);
         // //alert(JSON.stringify(instrument) + 'error showPayment ui show() then');
         // const status = tezResponse.Status;
