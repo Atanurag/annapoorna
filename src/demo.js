20d773d483dd3d6f73e9cb706b716059cd752823
Atanurag <anuragt9510@gmail.com>
2024-08-26 20:31:49 +0530

    dd

--- src/demo.js
+++ src/demo.js
@@ -490,7 +490,7 @@ function onBuyClicked() {
       data: {
         pa: '7875853859@pthdfc',
         pn: 'Anurag Tiwari-1',
-        tr: 'h28r3u54980De',// your custom transaction reference ID
+        tr: 'h28r3u549y80De',// your custom transaction reference ID
         url:'https://17174cc3-e036-41c5-82a6-1ce90c624cd6-00-2oq5i07bzmsdh.pike.repslit.dev:5000',
         mc: '5812', // your merchant category code
       },
