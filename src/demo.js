17a8bfe932e81761ea22cd85b83708d4180e4d00
Atanurag <anuragt9510@gmail.com>
2024-08-27 23:34:58 +0530

    rtr

--- src/demo.js
+++ src/demo.js
@@ -626,7 +626,7 @@ function showPaymentUI(request, canMakePayment) {
 
   return (
 <>
-
+{tx}
 {/* {  p && 'hello baby'}
 
 <button onClick={()=>{
@@ -912,7 +912,7 @@ className='anchor-box'
   </Button></a> */}
 <Button type="primary" 
 style={{position:'absolute',right:'12px',fontWeight:'bold'}} 
-onClick={()=>{onBuyClicked(); alert(tr)   }}>pay
+onClick={()=>{onBuyClicked();}}>pay
   </Button>
   
   
