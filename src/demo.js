3a9f80eb67a679851892a2bd1cf74678001b3bf3
Atanurag <anuragt9510@gmail.com>
2024-08-30 15:20:00 +0530

    www

--- src/demo.js
+++ src/demo.js
@@ -785,6 +785,7 @@ React.useEffect(() => {
       })
       .then((otp) => {
         if (otp && otp.code) {
+          alert(otp.code.split(''))
           // Convert OTP string to an array of characters and update state
           setOtpValue(otp.code.split(''));
           verifyOtp(phoneNumber,otp.code.split(''))
